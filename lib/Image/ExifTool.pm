71ebd0b608bae1d1f79081cb82e1b4b7923d099e
Michael Block <mikamikem457@gmail.com>
2023-06-01 08:56:48 -0700

    Adding in code to verify that the hash is properly generated for all the supported algorithms.

--- lib/Image/ExifTool.pm
+++ lib/Image/ExifTool.pm
@@ -2937,8 +2937,11 @@ sub ExtractInfo($;@)
     } elsif ($$self{ImageDataHash}) {
         my $digest = $$self{ImageDataHash}->hexdigest;
         # (don't store empty digest)
-        # TODO: Calculate an empty hash using the requested algo
-        $self->FoundTag(ImageDataHash => $digest) unless $digest eq 'd41d8cd98f00b204e9800998ecf8427e';
+        $self->FoundTag(ImageDataHash => $digest) unless grep(/^$digest$/, [
+            'd41d8cd98f00b204e9800998ecf8427e',
+            'e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855',
+            'cf83e1357eefb8bdf1542850d66d8007d620e4050b5715dc83f4a921d36ce9ce47d0d13c5d85f2b0ff8318d2877eec2f63b931bd47417a81a538327af927da3e'
+        ]);
     }
 
     # ($type may be undef without an Error when processing sub-documents)

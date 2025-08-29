def191453121fb7beb301fbf5ded5b758e968659
Hubert Figuière <hub@figuiere.net>
2025-08-28 20:55:20 -0400

    Added Ricoh GR IV model id
    Signed-off-by: Hubert Figuière <hub@figuiere.net>

--- lib/Image/ExifTool/Pentax.pm
+++ lib/Image/ExifTool/Pentax.pm
@@ -564,6 +564,7 @@ my %pentaxModelID = (
     0x1329a => 'GR IIIx', # (Ricoh)
     0x132b8 => 'KF', #github322 (Ricoh)
     0x132d6 => 'K-3 Mark III Monochrome', #github226 (Ricoh)
+    0x132e0 => 'GR IV', # (Ricoh)
 );
 
 # Pentax city codes - (PH, Optio WP)
